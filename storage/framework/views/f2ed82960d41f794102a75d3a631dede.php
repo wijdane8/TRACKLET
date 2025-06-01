d8f0bac03d4ee9db577e0c1433e4cdb055ac3174
wijdane8 <wijdane.ali@gmail.com>
2025-06-01 16:12:38 +0300

    pdate ke

---
+++ storage/framework/views/f2ed82960d41f794102a75d3a631dede.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/wijdan/Desktop/SAFFAPI-838cfb0a8bf7c6fe352e6d0a1f35c0a8a16ae898/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
